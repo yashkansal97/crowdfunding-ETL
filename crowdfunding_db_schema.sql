6564f986a5653bdca2907fd5f487eea6f1910e93
brittni.bresee@gmail.com <111385077+brittnibresee@users.noreply.github.com>
2023-03-09 22:45:04 -0600

    SQL_image_file

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE contacts (
+	contact_id INT PRIMARY KEY,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	email VARCHAR NOT NULL
+);
+CREATE TABLE category (
+	category_id VARCHAR PRIMARY KEY,
+	category VARCHAR NOT NULL
+);
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR PRIMARY KEY,
+	subcategory VARCHAR NOT NULL
+);
+
+CREATE TABLE campaign(
+	cf_id INT PRIMARY KEY,
+	contact_id INT NOT NULL,
+	company_name VARCHAR NOT NULL,
+	description VARCHAR NOT NULL,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
+	outcome VARCHAR NOT NULL,
+	backers_count INT NOT NULL,
+	country VARCHAR NOT NULL,
+	currency VARCHAR NOT NULL,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR NOT NULL,
+	subcategory_id VARCHAR NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
+	FOREIGN KEY (category_id) REFERENCES category (category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
+);
+
+
+
